eb23a52b33fca5b6f3fb8a4d79a30e771d9c98a9
David Wronek <david.wronek@mainlining.org>
2026-09-20 11:47:05 +0200

    create-disp: add updateScript
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- pkgs/create-disp/default.nix
+++ pkgs/create-disp/default.nix
@@ -6,6 +6,7 @@
   libdrm,
   pkg-config,
   systemd,
+  nix-update-script,
 }:
 let
   libhybris = callPackage ../libhybris { };
@@ -33,4 +34,16 @@ stdenv.mkDerivation {
     libhybris
     systemd
   ];
+
+  passthru = {
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--flake"
+        "--version"
+        "branch=master"
+        "--version-regex"
+        "(0-unstable.*)"
+      ];
+    };
+  };
 }
