55c7fe096eee59b4d6bc80313d5e491188f6b5e7
David Wronek <david.wronek@mainlining.org>
2026-09-21 21:17:25 +0200

    tmp
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- nixos/plasma-desktop.nix
+++ nixos/plasma-desktop.nix
@@ -2,7 +2,7 @@
 {
   services = {
     desktopManager.plasma6.enable = true;
-    displayManager.sddm.enable = true;
+    displayManager.plasma-login-manager.enable = true;
   };
 
   environment = {
